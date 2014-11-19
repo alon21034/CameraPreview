007c5c889becb93285c0e7c17cf20f45ca0bf9f7
Vince <lee790909@gmail.com>
2014-11-19 21:22:09 +0100

    basic camera

--- src/tw/alon/camerapreview/MainActivity.java
+++ src/tw/alon/camerapreview/MainActivity.java
@@ -1,15 +1,35 @@
 package tw.alon.camerapreview;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
 
+	private Camera mCamera;
+	private CameraPreview mPreview;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// Full screen
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+		        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 		setContentView(R.layout.activity_main);
+
+		// Create an instance of Camera
+		mCamera = getCameraInstance();
+
+		// Create our Preview view and set it as the content of our activity.
+		mPreview = new CameraPreview(this, mCamera);
+		FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+		preview.addView(mPreview);
 	}
 
 	@Override
@@ -19,4 +39,13 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public static Camera getCameraInstance() {
+		Camera c = null;
+		try {
+			c = Camera.open(); // attempt to get a Camera instance
+		} catch (Exception e) {
+			// Camera is not available (in use or does not exist)
+		}
+		return c; // returns null if camera is unavailable
+	}
 }
